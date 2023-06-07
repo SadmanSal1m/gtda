04e0ee3175dbd305062f60c83e5c44d8c59000d9
SadmanSal1m <106877035+SadmanSal1m@users.noreply.github.com>
2023-06-07 12:25:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from statsmodels.tsa.arima.model import ARIMA
 
+# Set page title and layout
+st.set_page_config(page_title='Global Terrorism Analysis', layout='wide')
+
 # Upload the dataset CSV file
 st.subheader('Upload Dataset')
 file = st.file_uploader("Upload CSV", type=["csv"])
 if file is not None:
     # Read the CSV file
     data = pd.read_csv(file, encoding='ISO-8859-1', low_memory=False)
-        
-# Set page title and layout
-st.set_page_config(page_title='Global Terrorism Analysis', layout='wide')
 
 # Sidebar widgets
 analysis_options = ['Prediction of Terrorist Attacks', 'Geospatial Analysis', 'Temporal Analysis',
